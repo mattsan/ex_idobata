94c2cd2aa46ddd47c8abca2c684c53d8cc20420a
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-09-08 10:07:35 +0900

    add functions to post to Idobata.io

--- lib/ex_idobata.ex
+++ lib/ex_idobata.ex
@@ -1,18 +1,28 @@
 defmodule ExIdobata do
-  @moduledoc """
-  Documentation for ExIdobata.
-  """
+  defstruct [:url]
 
-  @doc """
-  Hello world.
+  def new_hook(url) do
+    %ExIdobata{url: url}
+  end
 
-  ## Examples
+  def post_message(%ExIdobata{url: url}, message, options \\ []) do
+    headers = ["Content-Type": "application/x-www-form-urlencoded"]
 
-      iex> ExIdobata.hello()
-      :world
+    encoded_message = URI.encode_www_form(message)
+    body = if get_in(options, [:html]) do
+        "source=#{encoded_message}&format=html"
+      else
+        "source=#{encoded_message}"
+      end
 
-  """
-  def hello do
-    :world
+    HTTPoison.post(url, body, headers)
+  end
+
+  def post_image_file(%ExIdobata{url: url}, filename) do
+    HTTPoison.post(url, muptipart_image(filename))
+  end
+
+  defp muptipart_image(filename) do
+    {:multipart, [{:file, filename, {"form-data", [{"name", "image"}, {"filename", Path.basename(filename)}]}, []}]}
   end
 end
--- test/ex_idobata_test.exs
+++ test/ex_idobata_test.exs
@@ -1,8 +1,4 @@
 defmodule ExIdobataTest do
   use ExUnit.Case
   doctest ExIdobata
-
-  test "greets the world" do
-    assert ExIdobata.hello() == :world
-  end
 end
