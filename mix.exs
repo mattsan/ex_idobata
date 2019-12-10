23d5f351b59f0c554863851a22e62e4da0edbfb2
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-12-10 21:27:45 +0900

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExIdobata.MixProject do
   def project do
     [
       app: :ex_idobata,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(),
